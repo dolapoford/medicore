a3a663f67441cce36dbbc909c2b06305322cd089
dolapoford <ahmadasiyanbola04@gmail.com>
2026-04-29 10:16:05 -0700

    Doctor Dto updated

--- src/main/java/com/example/medicore/dto/request/DoctorRequestDTO.java
+++ src/main/java/com/example/medicore/dto/request/DoctorRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.medicore.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -12,6 +13,10 @@ public class DoctorRequestDTO {
 
     @NotBlank
     private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
 
     @Pattern(regexp = "^\\+?[0-9]{7,15}$", message = "Invalid Phone number")
     private String phoneNumber;
