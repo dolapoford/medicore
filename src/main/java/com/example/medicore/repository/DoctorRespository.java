fd740f0109cf024c931a87e0d1414c34b3852d86
dolapoford <ahmadasiyanbola04@gmail.com>
2026-04-29 10:17:07 -0700

    Doctor repo  updated

--- src/main/java/com/example/medicore/repository/DoctorRespository.java
+++ src/main/java/com/example/medicore/repository/DoctorRespository.java
@@ -1,6 +1,8 @@
 package com.example.medicore.repository;
 
 import com.example.medicore.entity.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +13,5 @@ import java.util.Optional;
 public interface DoctorRespository extends JpaRepository<Doctor,Long> {
     Optional<Doctor> findByEmail(String email);
     boolean existsByEmail(String email);
-    List<Doctor> findBySpecialization(String specialization);
+    Page<Doctor> findBySpecialization(String specialization, Pageable pageable);
 }
