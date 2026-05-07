a5096836b19442d0d27527eb53f9a32bf9e09eb5
dolapoford <ahmadasiyanbola04@gmail.com>
2026-05-07 03:50:42 -0700

    added existBydoctorId to check an existing doctor

--- src/main/java/com/example/medicore/repository/UserRepository.java
+++ src/main/java/com/example/medicore/repository/UserRepository.java
@@ -16,4 +16,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByRefreshToken(String refreshToken);
 
     Optional<User> findByUsernameIgnoreCaseOrEmailIgnoreCase(String username, String email);
+
+    boolean existsByDoctorId(Long id);
 }
