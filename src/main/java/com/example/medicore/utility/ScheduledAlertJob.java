5cd0693aa394d7eb28b8b38775391e001367801b
dolapoford <ahmadasiyanbola04@gmail.com>
2026-05-12 08:18:34 -0700

    Alert working

---
+++ src/main/java/com/example/medicore/utility/ScheduledAlertJob.java
@@ -0,0 +1,38 @@
+package com.example.medicore.utility;
+
+import com.example.medicore.entity.DeviceReading;
+import com.example.medicore.repository.DeviceReadingRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScheduledAlertJob {
+
+    private final DeviceReadingRepository deviceReadingRepository;
+
+    @Scheduled(cron = "0 */5 * * * *")
+    public void scanRecentAnomalies(){
+        List<DeviceReading> recentAnomalies = deviceReadingRepository.findByIsAnomalyTrueAndRecordedAtAfter(
+                java.time.LocalDateTime.now().minusMinutes(5)
+        );
+
+
+        if (recentAnomalies.isEmpty()){
+            log.info("no anomalies found");
+        }
+
+        for (DeviceReading anomaly : recentAnomalies){
+            log.warn("Anomaly detected - Device Id: {}, Type:{}, Value: {} {}", anomaly.getDevice().getId(), anomaly.getDevice().getDeviceType(),anomaly.getValue(),anomaly.getUnit());
+        }
+
+
+    }
+
+
+}
